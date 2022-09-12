08fc7b03c1597080d76460e7943e4b1db7618f5c
besed <besed@DESKTOP-472DDVS>
2022-09-12 23:06:35 +0400

    Add project files.

---
+++ Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.RegularExpressions;
+
+namespace regex
+{ 
+    class Strings 
+    {
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("Введите предложение");
+            string sentence = Console.ReadLine();
+
+            
+            string prefixReplace = Regex.Replace(sentence, @"([^а-я]|^)(п)ри([а-я]+([!.?]|$))","$1$2ре$3",RegexOptions.IgnoreCase);
+            string flexionReplace = Regex.Replace(prefixReplace, @"(([!.?]\s*|^)[а-я]+)ие([^а-я]|$)", "$1ее$3", RegexOptions.IgnoreCase);
+            Console.WriteLine(flexionReplace);
+
+        }
+
+    }
+
+
+}
+
