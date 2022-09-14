0cbed4985fee0952de49393c4965f164a430343a
Ksusha Osipova <besedinaksenya@mail.ru>
2022-09-14 22:04:28 +0400

    add archivation

---
+++ Archivation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+using System.Diagnostics;
+
+namespace archivation
+{
+    class Archivation
+    {
+        static void Main(string[] args)
+        {
+            string dirMain = "C:\\Users\\besed\\Каталог";
+
+            if (!Directory.Exists(dirMain))
+            {
+                throw new Exception("The directory does not exist");
+            }
+            string[] files = Directory.GetFiles(dirMain);
+            string comprFile = "C:\\Users\\besed\\archive.gz";
+            
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+
+            var task = new Task(() => 
+            {
+                using FileStream targetStream = File.Create(comprFile);
+                using GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress);
+                foreach (string file in files)
+                {
+                    token.ThrowIfCancellationRequested();
+                    using FileStream sourceStream = new FileStream(file, FileMode.Open);
+                    sourceStream.CopyTo(compressionStream);
+                    Console.WriteLine(file);
+                    Thread.Sleep(2000);
+                }
+            }, token);
+            task.Start();
+            //task.Wait();
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    if (task.IsCompleted)
+                        return;
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            cancelTokenSource.Cancel();
+            task.Wait();
+
+
+        }
+
+    }
+}
+
---
+++ DateSort/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DateSort
+{
+    public class DateSort
+    {
+        private readonly Dictionary<string, DateTime> listPeople = new Dictionary<string, DateTime>()
+
+        public void ReadFile(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                while (!file.EndOfStream)
+                {
+                    var fileLine = file.ReadLine();
+                    var fragments = fileLine.Split('#', 2);
+                    string key = fragments[0];
+                    DateTime value = fragments[1];
+                    listPeople[key] = value;
+                }
+
+            }
+        } 
+    } 
+}
